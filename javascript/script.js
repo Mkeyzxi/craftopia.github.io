d0e66a5d3bd3bd39cd6fbc256dc0f70d677cd47a
Muhammad Makbul N <muhmakbul6@gmail.com>
2023-12-30 21:57:58 +0800

    menambahkan animasi loading pada landing page dan menambahkan icon kontak serta memodifikasi css untuk animasi

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,28 @@
 const parent = document.body;
 const headerNav = document.querySelector('header nav');
 const headerContact = document.querySelector(' header .contact');
+const toggleMenu = document.getElementById('toggleMenu');
 const span1 = parent.querySelector('header .toggleMenu span:nth-child(1)');
 const span2 = parent.querySelector('header .toggleMenu span:nth-child(2)');
 const span3 = parent.querySelector('header .toggleMenu span:nth-child(3)');
+const sectionSatuHome = parent.querySelector('main section.sectionSatu .home');
+const sectionSatuImg = parent.querySelector('main section.sectionSatu img');
+// const sectionDua = parent.querySelector('main section.sectionDua');
 const linkMulaiEkspor = parent.querySelector('main section.sectionSatu a');
 let isOpen = false;
-
+// responsive mobile
 function toggleFunction() {
     isOpen = !isOpen; // Mengubah nilai isOpen menjadi kebalikannya
 
     if (isOpen) {
-        // Jika menu terbuka
-        span2.style.backgroundColor = 'rgba(0,0,0,0)';
+        // Jika menu terbuka dalam mobile
         span1.style.transform = 'rotate(45deg)';
-        span3.style.transform = 'rotate(-45deg)';
         span1.style.marginTop = '11px';
+        span1.style.transition = 'all .2s ease-in 0s';
+        span2.style.transition = 'all .2s ease-in 0s';
+        span2.style.backgroundColor = 'rgba(0,0,0,0)';
+        span3.style.transform = 'rotate(-45deg)';
         span3.style.marginTop = '-16px';
-        span1.style.transition = 'all .1s ease-in 0s';
         headerNav.style.display = 'block';
         headerContact.style.display = 'flex';
 
@@ -33,7 +38,12 @@ function toggleFunction() {
 
     }
 }
-
+// ketika link diklik kata berubah
 linkMulaiEkspor.onclick = function() {
     linkMulaiEkspor.textContent = 'Gasskan';
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    sectionSatuHome.classList.add('loadingAnimasiSectionSatuHome');
+    sectionSatuImg.classList.add('loadingAnimasiSectionSatuImg');
+});
